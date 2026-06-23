e4c22aecd473e98fc2cbad526058130c88502b4a
Nina <ninapankninberlin@gmail.com>
2026-06-23 09:51:49 +0200

    fix /wetter: hero photo vibrancy boost (brighter, livelier by day) + sw v8

--- wetter/sw.js
+++ wetter/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "aether-v7";
+const CACHE = "aether-v8";
 const SHELL = ["./","index.html","css/styles.css","js/app.js","manifest.json","img/pablo.png"];
 self.addEventListener("install", e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL))); });
 self.addEventListener("activate", e => e.waitUntil(
